8267825b08fd316ce28639a45423742153bc0d82
MovePlayCreate <movecreateplay@gmail.com>
2025-09-07 07:44:36 -0400

    Update ex16.py

--- ex16.py
+++ ex16.py
@@ -1,4 +1,8 @@
+# play with pandas with this file
+import pandas
+data = "data.csv"
 filename = "test.txt"
+
 
 print(f"We're going to erase {filename}.")
 print("If you don't want that, hit CTRL-C (^C).")
